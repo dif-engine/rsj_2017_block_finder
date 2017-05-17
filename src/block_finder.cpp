4efcc50fcdd4f2ab1bea15659764ea620a0db7b8
Takuo Suzuki <takuo.suzuki@ist.aichi-pu.ac.jp>
2017-05-17 23:34:24 +0900

    tf publish

--- src/block_finder.cpp
+++ src/block_finder.cpp
@@ -15,12 +15,12 @@
 #include <pcl_ros/transforms.h>//#include <Eigen/Geometry>
 #include <image_geometry/pinhole_camera_model.h>//カメラモデルを利用するため
 
-using namespace std;
-using namespace cv;
+//using namespace std;
+//using namespace cv;
 
-static const string WINDOW_O = "Original";
-static const string WINDOW_T = "Temp";
-static const string WINDOW_R = "Result";
+static const std::string WINDOW_O = "Original";
+static const std::string WINDOW_T = "Temp";
+static const std::string WINDOW_R = "Result";
 static int threshold_bin = 160;
 
 class BlockFinder
@@ -35,46 +35,35 @@ class BlockFinder
 	tf::TransformBroadcaster tf_broadcaster_;
 
 	image_geometry::PinholeCameraModel cam_model_;
-	
+
 	std::string fixed_frame;
 	std::string camera_frame;
 	std::string target_frame;
 
-	cv::Mat ideal_points_;
+	cv::Matx33d K;//3x3の行列で、要素はdouble
+	cv::Mat D;
 
 public:
 
-
-	BlockFinder(): it(nh_)
-{
-		image_sub_ = it.subscribe("/usb_cam_node/image_raw", 1, &BlockFinder::imageCb, this);
-		info_sub_ = nh_.subscribe("/usb_cam/camera_info", 1, &CalibrateKinectCheckerboard::infoCallback, this);
-		image_pub_ = it.advertise("/block_finder/image_block", 1);
-		pose_pub_ = nh_.advertise<geometry_msgs::Pose2D>("/block_finder/pose", 1);		
-
-		nh_.param<std::string>("fixed_frame", fixed_frame, "/base_link");
-		nh_.param<std::string>("camera_frame", camera_frame, "/camera_link");
-		nh_.param<std::string>("target_frame", target_frame, "/calibration_pattern");
-
-		namedWindow(WINDOW_O);
-		namedWindow(WINDOW_T);
-		namedWindow(WINDOW_R);
-		moveWindow(WINDOW_O, 0, 0);
-		moveWindow(WINDOW_T, 640, 0);
-		moveWindow(WINDOW_R, 1280, 0);
-		createTrackbar("Subtracter", WINDOW_T, &threshold_bin, 255);
-}
-
-	~BlockFinder()
+	void convertCVtoEigen(cv::Mat& tvec, cv::Mat& R, Eigen::Vector3f& translation, Eigen::Quaternionf& orientation)
 	{
-		destroyWindow(WINDOW_O);
-		destroyWindow(WINDOW_T);
-		destroyWindow(WINDOW_R);
+		// This assumes that cv::Mats are stored as doubles. Is there a way to check this?
+		// Since it's templated...
+		translation = Eigen::Vector3f(tvec.at<double>(0, 0), tvec.at<double>(0, 1), tvec.at<double>(0, 2));
+
+		Eigen::Matrix3f Rmat;
+		Rmat << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2),
+				R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2),
+				R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2);
+
+		orientation = Eigen::Quaternionf(Rmat);
 	}
 
+
+	//正解となるチェッカーボードの３次元位置を計算する。
 	cv::Mat calcChessboardCorners(cv::Size boardSize, float squareSize, cv::Point3f offset)
 	{
-		Mat corners;
+		cv::Mat corners;
 
 		for (int i = 0; i < boardSize.height; i++)
 			for (int j = 0; j < boardSize.width; j++)
@@ -84,17 +73,43 @@ public:
 	}
 
 
-	  void infoCallback(const sensor_msgs::CameraInfoConstPtr& info_msg)
-	  {
-	    cam_model_.fromCameraInfo(info_msg);
-	    //pattern_detector_.setCameraMatrices(cam_model_.intrinsicMatrix(), cam_model_.distortionCoeffs());
+	void infoCallback(const sensor_msgs::CameraInfoConstPtr& info_msg)
+	{
+		cam_model_.fromCameraInfo(info_msg);
 
-	    //calibrated = true;
-	    //image_sub_ = nh_.subscribe("/camera/rgb/image_mono", 1, &CalibrateKinectCheckerboard::imageCallback, this);
+		K = cam_model_.intrinsicMatrix();
+		D = cam_model_.distortionCoeffs();
+	}
 
-	    ROS_INFO("[calibrate] Got image info!");
-	  }
-	
+
+	BlockFinder(): it(nh_)
+	{
+		info_sub_ = nh_.subscribe("/usb_cam_node/camera_info", 1, &BlockFinder::infoCallback, this);
+		image_sub_ = it.subscribe("/usb_cam_node/image_raw", 1, &BlockFinder::imageCb, this);
+		image_pub_ = it.advertise("/block_finder/image_block", 1);
+		pose_pub_ = nh_.advertise<geometry_msgs::Pose2D>("/block_finder/pose", 1);		
+
+		nh_.param<std::string>("fixed_frame", fixed_frame, "/base_link");
+		nh_.param<std::string>("camera_frame", camera_frame, "/camera_link");
+		nh_.param<std::string>("target_frame", target_frame, "/calibration_pattern");
+
+		cv::namedWindow(WINDOW_O);
+		cv::namedWindow(WINDOW_T);
+		cv::namedWindow(WINDOW_R);
+		cv::moveWindow(WINDOW_O, 0, 0);
+		cv::moveWindow(WINDOW_T, 640, 0);
+		cv::moveWindow(WINDOW_R, 1280, 0);
+		cv::createTrackbar("Subtracter", WINDOW_T, &threshold_bin, 255);
+	}
+
+	~BlockFinder()
+	{
+		cv::destroyWindow(WINDOW_O);
+		cv::destroyWindow(WINDOW_T);
+		cv::destroyWindow(WINDOW_R);
+	}
+
+
 	void imageCb(const sensor_msgs::ImageConstPtr& msg)
 	{
 		geometry_msgs::Pose2D pose_block;
@@ -102,85 +117,90 @@ public:
 		pose_block.y = 0;
 		pose_block.theta = 0;
 
-		Size patternsize(8,6);
-		vector<Point2f> board_corners;
+		cv::Size patternsize(8,6);
+		cv::Mat board_corners;
 
 		cv_bridge::CvImagePtr cv_ptr_, cv_ptr_2;
 		try
 		{
 			cv_ptr_ = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-			cv_ptr_2 = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 		}
 		catch (cv_bridge::Exception& ex)
 		{
-			ROS_ERROR("cv_bridge exception: %s", ex.what());
+			ROS_ERROR("cv_bridge exception:\n%s", ex.what());
 			return;
 		}
 
-		Mat img_gray_;
+		cv::Mat img_gray_;
 		cvtColor(cv_ptr_->image, img_gray_, CV_BGR2GRAY);
 
-		bool patternfound = findChessboardCorners(img_gray_, patternsize, board_corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+		bool patternfound = findChessboardCorners(img_gray_, patternsize, board_corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
 
-		drawChessboardCorners(img_gray_, patternsize, cv::Mat(board_corners), patternfound);
-
-		tf::Transform target_transform;
-		tf::StampedTransform base_transform;
-		try
+		if(patternfound)
 		{
-			ros::Time acquisition_time = msg->header.stamp;
-			ros::Duration timeout(1.0 / 30.0);
-			if(patternfound)
+			cv::drawChessboardCorners(img_gray_, patternsize, board_corners, patternfound);
+			
+			try
 			{
-				
-				Eigen::Vector3f translation(0.0, 0.0, 0.0);
-				Eigen::Quaternionf orientation(0.0, 0.0, 0.0, 0.0);
-				cv::Mat observation_points_ = board_corners;
-				cv::Matx33d K;// 3x3の行列で、要素はdouble
-				cv::Mat D;// 
-				cv::Mat rvec, tvec;//座標を指すベクトル
-				
-				
-				ideal_points_ = calcChessboardCorners(cv::Size(8, 6), 0.025, cv::Point3f(0.0,0.0,0.0));
+					tf::Transform target_transform;
+					tf::StampedTransform base_transform;
+					
+					Eigen::Vector3f translation(0.0, 0.0, 0.0);
+					Eigen::Quaternionf orientation(0.0, 0.0, 0.0, 0.0);
+					cv::Mat observation_points_ = board_corners;
+					cv::Mat tvec;//並進ベクトル
+					cv::Mat rvec;//回転行列
+					cv::Mat R;
 
-				//画像上の２次元位置と空間内の３次元位置を対応付ける。
-				cv::solvePnP(cv::Mat(ideal_points_), cv::Mat(observation_points_), K, D, rvec, tvec, false);
-				translation = Eigen::Vector3f(tvec.at<double>(0, 0), tvec.at<double>(0, 1), tvec.at<double>(0, 2));
+					//
+					cv::Mat ideal_points_ = calcChessboardCorners(cv::Size(8, 6), 0.025, cv::Point3f(0.0,0.0,0.0));
 
-				//target_transform.setOrigin(tf::Vector3(translation.x(), translation.y(), translation.z()));
-				//target_transform.setRotation(tf::Quaternion(orientation.x(), orientation.y(), orientation.z(), orientation.w()));
-				tf_broadcaster_.sendTransform(tf::StampedTransform(target_transform, msg->header.stamp, msg->header.frame_id, target_frame));
+					//画像上の２次元位置と空間内の３次元位置を対応付ける。
+					cv::solvePnP(ideal_points_, observation_points_, K, D, rvec, tvec, false);//cv::solvePnP(cv::Mat(ideal_points_), cv::Mat(observation_points_), K, D, rvec, tvec, false);
+					cv::Rodrigues(rvec, R);
+					convertCVtoEigen(tvec, R, translation, orientation);
+
+					//変換ベクトルを確認する。
+					ROS_INFO("%5.3f, %5.3f, %5.3f", translation.x(), translation.y(), translation.z());
+
+					//変換ベクトルを登録する。
+					target_transform.setOrigin(tf::Vector3(translation.x(), translation.y(), translation.z()));
+					target_transform.setRotation(tf::Quaternion(orientation.x(), orientation.y(), orientation.z(), orientation.w()));
+
+					//usb_camの原点はhead_camera
+					tf_broadcaster_.sendTransform(tf::StampedTransform(target_transform, ros::Time::now(), "head_camera", target_frame));
+			}
+			catch (tf::TransformException& ex)
+			{
+				ROS_WARN("TF exception:\n%s", ex.what());
+				return;
 			}
 		}
-		catch (tf::TransformException& ex)
+		else
 		{
-			ROS_WARN("[calibrate] TF exception:\n%s", ex.what());
-			return;
+			ROS_INFO("Not Found");
 		}
-
-		/*
+		
 		//Binarization
-		Mat img_gray;
-		Mat img_bin, img_bin2;
-		cvtColor(cv_ptr->image, img_gray, CV_BGR2GRAY);
-		medianBlur(img_gray, img_gray, 3);
-		threshold(img_gray, img_bin, threshold_bin, 255, THRESH_BINARY);//threshold(img_gray, img_bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
+		cv::Mat img_bin_, img_bin_2;
+		cv::medianBlur(img_gray_, img_gray_, 3);
+		cv::threshold(img_gray_, img_bin_, threshold_bin, 255, cv::THRESH_BINARY);//threshold(img_gray, img_bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
 
 		//Contour Line
-		vector<vector<Point> > contours;//輪郭を表現するベクトル
-		img_bin2 = img_bin.clone();
-		findContours(img_bin2, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		std::vector<std::vector<cv::Point> > contours;//輪郭を表現するベクトル
+		img_bin_2 = img_bin_.clone();
+		cv::findContours(img_bin_2, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 		if(contours.empty() != true){
 			double max_area=0;
 			int max_area_contour=-1;
 			for(int i=0;i<contours.size();i++)
 			{
-				double area=contourArea(contours.at(i));
+				double area=cv::contourArea(contours.at(i));
 				if(max_area<area){
 					max_area=area;
 					max_area_contour=i;
 				}
-				polylines(cv_ptr->image, contours.at(i), true, Scalar(0, 255, 0), 3);
+				polylines(cv_ptr_->image, contours.at(i), true, cv::Scalar(0, 255, 0), 3);
 			}
 
 			//Gravity Point
@@ -193,9 +213,8 @@ public:
 			}
 			x/=count;
 			y/=count;
-			circle(cv_ptr->image, Point(x,y), 10, Scalar(0,0,255), -1, CV_AA);
+			circle(cv_ptr_->image, cv::Point(x,y), 10, cv::Scalar(0,0,255), -1, CV_AA);
 		}
-		 */
 
 		//Segmentation
 		/*
@@ -223,12 +242,12 @@ public:
 		 */
 
 		//結果の表示
-		imshow(WINDOW_O, cv_ptr_->image);//処理前
-		waitKey(10);
-		imshow(WINDOW_T, img_gray_);//処理中
-		waitKey(10);
-		//imshow(WINDOW_R, cv_ptr_2->image);//処理後
-		waitKey(10);
+		cv::imshow(WINDOW_O, cv_ptr_->image);//処理前
+		cv::waitKey(10);
+		cv::imshow(WINDOW_T, img_gray_);//処理中
+		cv::waitKey(10);
+		//cv::imshow(WINDOW_R, cv_ptr_->image);//処理後
+		//cv::waitKey(10);
 
 		//結果の出力
 		image_pub_.publish(cv_ptr_->toImageMsg());
